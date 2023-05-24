9477cd3e6f40e8144c3c3b9f2ed010743f8184be
sonybob <kutsmg@gmail.com>
2023-05-24 11:27:11 +0300

    New commit

---
+++ lesson1.js
@@ -0,0 +1,56 @@
+// /*
+// const user = {
+//     name: "Mike",
+//     login: "Login@gmail.com",
+//     email: "MyLogin",
+//     pass: 12345,
+//     adress: {
+//         street: "shevchenka",
+//         zip: 12234,
+//         apartnum: 37
+//     }
+// }
+// user.adress.phone = {
+//     code: "+380",
+//     number: 7777777
+// }
+
+// user.adress['phone'].number = 55555;
+// delete user.adress.zip;
+// user.email="kuku@gmail.com";
+// user["login"] = "kukushka";
+// user["adress"].phone.code = "+9530";
+// console.log(user); 
+// ___________________________________________________________________________________________________________
+// */
+
+// // const array = ["jhon",{name:"Osel", age: 17}, {name:"Baran", age:21}, {name:"Pituh", age:40}];
+// // array[1].name = "Loh";
+// // delete array[0].age;
+// // array[array.length] = {name:"bomz", age:99};
+// // array.push ({name: "Gus", age:700});
+// // array.pop();
+// // array[0] = "jesica";
+// // console.log(array);
+// //____________________________________________________________________________________________________________
+// // const myMap =  new Map([[true,"bool"], [12345 ,"09876"], ['obj' ,{name:"Loh", age: 21}]]);
+
+// // myMap.set(+380, 777777);
+// // myMap.set("obj1", {name: "bob", age: 60});
+// // myMap.set(12345, 54321)
+// // myMap.delete(true);
+
+// // console.log(myMap);
+// // console.log(myMap.has(12345));
+// // console.log(myMap.get(12345));
+// // console.log(myMap.get("obj"));
+// // console.log(myMap.size);
+// //___________________________________________________________________________________________________________
+// const mySet = new Set(['bool', 'newtrue',"bool", 12345 ,"09876", {name:"Loh", age: 21}]);
+// mySet.add(12345);
+// mySet.add(123);
+// mySet.delete('bool');
+
+// console.log(mySet);
+// console.log(mySet.has(123));
+// console.log(mySet.size);
